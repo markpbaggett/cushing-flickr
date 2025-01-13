8f5e62f873a4d2dc4471b3039947222a088c74e1
Mark Baggett <markpbaggett@gmail.com>
2025-01-13 15:44:00 -0600

    Init.

---
+++ src/cushing.py
@@ -0,0 +1,70 @@
+import flickrapi
+import os
+import json
+
+
+class FlickrConnection:
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = secret
+        self.connect = self._connect()
+
+    def _connect(self):
+        try:
+            return flickrapi.FlickrAPI(self.key, self.secret, format='parsed-json')
+        except Exception as e:
+            raise ConnectionError(f"Error connecting to Flickr API: {e}")
+
+
+class CushingImage:
+    def __init__(self, photo_id, connection):
+        self.photo_id = photo_id
+        self.connection = connection
+
+    def get_info(self):
+        all_metadata = self.connection.photos.getInfo(photo_id=self.photo_id)
+        descriptive_key_values = all_metadata['photo']['description']['_content'].split('\n')
+        all_metadata['photo']['descriptive_metadata'] = {}
+        for item in descriptive_key_values:
+            no_bold = item.replace('<b>', '').replace('</b>', '')
+            key, *values = no_bold.split(":")
+            final_value = ':'.join(values).strip()
+            if key != "":
+                all_metadata['photo']['descriptive_metadata'][key] = final_value
+        return all_metadata
+
+    def get_exif(self):
+        return self.connection.photos.getExif(photo_id=self.photo_id)
+
+    def get_comments(self):
+        return self.connection.photos.comments.getList(photo_id=self.photo_id)
+
+    def get_sizes(self):
+        return self.connection.photos.getSizes(photo_id=self.photo_id)
+
+    def get_all(self):
+        return {
+            'metadata': self.get_info(),
+            'exif': self.get_exif(),
+            'comments': self.get_comments(),
+            'sizes': self.get_sizes()
+        }
+
+    def get_all_as_json(self):
+        return json.dumps(self.get_all(), indent=4)
+
+
+def main():
+    flickr_key = os.getenv('FLICKR_KEY')
+    flickr_secret = os.getenv('FLICKR_SECRET')
+
+    if not flickr_key or not flickr_secret:
+        raise ValueError("FLICKR_KEY and FLICKR_SECRET environment variables must be set")
+
+    connection = FlickrConnection(flickr_key, flickr_secret).connect
+    sample = CushingImage(photo_id='6220298977', connection=connection)
+    print(sample.get_all_as_json())
+
+
+if __name__ == '__main__':
+    main()
